23349c4fe9bc44b1461c93ea997b048cdc756dc9
del70806 <62988853+del70806@users.noreply.github.com>
2023-01-31 18:52:07 +0530

    service impl changes

--- marketing-lead2loyalty/apps-lead2loyalty/core/src/main/java/com/deloitte/aem/lead2loyalty/core/service/utility/impl/Lead2loyaltyServiceImpl.java
+++ marketing-lead2loyalty/apps-lead2loyalty/core/src/main/java/com/deloitte/aem/lead2loyalty/core/service/utility/impl/Lead2loyaltyServiceImpl.java
@@ -1,64 +1,27 @@
 package com.deloitte.aem.lead2loyalty.core.service.utility.impl;
 
-import com.deloitte.aem.lead2loyalty.core.beans.APIResponse;
-import com.deloitte.aem.lead2loyalty.core.beans.BaseRequest;
 import com.deloitte.aem.lead2loyalty.core.service.utility.Lead2loyaltyService;
-import com.deloitte.aem.lead2loyalty.core.service.utility.RestClient;
-import com.deloitte.aem.lead2loyalty.core.service.utility.configs.RestClientConfigurations;
-import com.deloitte.aem.lead2loyalty.core.util.RestServiceConstants;
-import com.deloitte.aem.lead2loyalty.core.util.RestServiceUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.MethodNotSupportedException;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.*;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContexts;
-import org.apache.http.ssl.TrustStrategy;
-import org.apache.http.util.EntityUtils;
-import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.metatype.annotations.Designate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The Class RestServiceImpl. The Rest Client uses
- * PoolingHttpClientConnectionManager to create the ClosableHttpClient THe
- * Connection is set with max connections with 200
+ * The Class Lead2loyaltyServiceImpl.
  */
-@Component(service = RestClient.class, immediate = true)
+@Component(service = Lead2loyaltyService.class, immediate = true)
 public class Lead2loyaltyServiceImpl implements Lead2loyaltyService {
 
     /**
      * The Constant LOG.
      */
-    private static final Logger LOG = LoggerFactory.getLogger(RestClientImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Lead2loyaltyServiceImpl.class);
 
     @Reference
     ResourceResolverFactory resourceResolverFactory;
