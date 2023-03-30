2523adb78764501e41e877356ef34a1ec01e38b3
nkumartamrakar <nkumartamrakar@deloitte.com>
2023-03-30 20:29:01 +0530

    Search pagination changes.

--- marketing-lead2loyalty/apps-lead2loyalty/ui.apps/src/main/content/jcr_root/apps/lead2loyalty/components/content/clientlibs/lead2loyalty-homepage/js/lead2loyalty.js
+++ marketing-lead2loyalty/apps-lead2loyalty/ui.apps/src/main/content/jcr_root/apps/lead2loyalty/components/content/clientlibs/lead2loyalty-homepage/js/lead2loyalty.js
@@ -1,26 +1,32 @@
 $( document ).ready(function() {
     var counter = 0;
-     $('#search-button1').click(function() {
-       searchFunc();
+    $('#search-button1').click(function() {
+       searchFunc(0);
     });
 
-    var input = document.getElementById("search");
-	input.addEventListener("keypress", function(event) {
+    /*var input = document.getElementById("search");
+    input.addEventListener("keypress", function(event) {
         if (event.key === "Enter") {
-        event.preventDefault();
-        searchFunc();
-      }
-    });
+            event.preventDefault();
+            searchFunc();
+        }
+    });*/
 
-    function searchFunc() {
+    $(function() {
+        $('.pagination').on('click', 'li', function(){
+            var offset = $('a', this).attr('offset');
+			searchFunc(Number(offset));
+        });
+	});
+
+    function searchFunc(offset) {
         let data = document.getElementById("search").value;
         var payload = {
-        	query: data,
-            offSet: "0"
+        	query: data
     	};
 
         if(payload.query !== "") {
-            if(counter >0) {
+            if(counter > 0) {
                 var handles = document.querySelectorAll("#filter-counts, #searchSection, #pagination");
 				for(var i = 0; i < handles.length; ++i) {
 		           handles[i].innerHTML = '';
@@ -35,22 +41,24 @@ $( document ).ready(function() {
                         var result = JSON.parse(resultData);
                         var resultList = JSON.parse(resultData).searchResultBeanList;
                         var filterList = JSON.parse(resultData).searchFilterBeanList;
-                        console.log(filterList);
                         $("#resultCount").text("");
                         $("#resultCount").append("<b>" + result.searchResultCount + " results found for " +'"'+ payload.query +'"' + "</b>");
 
-                        if(result.pageCount > 2) {
-                            console.log(result.pageCount);
+                        //Pagination
+                        if(result.pageCount >= 1) {
                             for(var i=1; i<=result.pageCount; i++) {
-                                $("#pagination").append('<li class="page-item"><a class="page-link" offSet='+i*10+' href="#">'+i+'</a></li>');
+                                $("#pagination").append('<li class="page-item"><a class="page-link" offSet='+(i-1)*10+' href="#">'+i+'</a></li>');
                             }
                         }
+                        //Search Filters
                         for(var i=0; i<filterList.length; i++) {
                             let newdiv = document.createElement('li');
                             newdiv.innerHTML = '<a href="#">'+filterList[i].filterTitle+' <span class="badge bg-primary">'+filterList[i].filterCount+'</span></a>';
                             document.getElementById('filter-counts').appendChild(newdiv);
                         }
-                        for(var i=0; i<resultData.length; i++) {
+                        //Render search result section
+                        var limit = resultList.length < 10 ? resultList.length : (offset + 10);
+                        for(var i=offset; i<limit; i++) {
                             let newDiv = document.createElement('section');
                             newDiv.innerHTML = '<div class="search-result-item"> <a class="image-link" href="'+resultList[i].pagePath+'.html" target="_blank"><img class="image" src="'+resultList[i].imagePath+'"></a><div class="search-result-item-body"><div class="row"><div class="col-sm-9"><h5 class="search-result-item-heading"><a href="#"> '+resultList[i].title+' </a></h5><p class="info">'+resultList[i].publishDate+'</p><p class="description">'+resultList[i].description+'</p><div class="custom-buttons"><span class="badge ">'+resultList[i].keywords+'</span></div></div></div></div></section>';
                             document.getElementById('searchSection').appendChild(newDiv);
@@ -73,20 +81,21 @@ $( document ).ready(function() {
                     $("#resultCount").text("");
                     $("#resultCount").append("<b>" + result.searchResultCount + " results found for " +'"'+ payload.query +'"' + "</b>");
 
-                    if(result.pageCount > 2) {
-                        console.log(result.pageCount);
+                    //Pagination
+                    if(result.pageCount >= 1) {
                         for(var i=1; i<=result.pageCount; i++) {
-                            $("#pagination").append('<li class="page-item"><a class="page-link" offSet='+i*10+' href="#">'+i+'</a></li>');
+                            $("#pagination").append('<li class="page-item"><a class="page-link" offSet='+(i-1)*10+' href="#">'+i+'</a></li>');
                         }
                     }
-
+                    //Search Filters
                     for(var i=0; i<filterList.length; i++) {
                         let newdiv = document.createElement('li');
                         newdiv.innerHTML = '<a class="badge-class" href="#">'+filterList[i].filterTitle+' <span class="badge bg-primary">'+filterList[i].filterCount+'</span></a>';
                         document.getElementById('filter-counts').appendChild(newdiv);
                     }
-
-                    for(var i=0; i<resultData.length; i++) {
+                    //Render search result section
+					var limit = resultList.length < 10 ? resultList.length : (offset + 10);
+                    for(var i=offset; i<limit; i++) {
                         let newDiv = document.createElement('section');
                         newDiv.innerHTML = '<div class="search-result-item"> <a class="image-link" href="'+resultList[i].pagePath+'.html" target="_blank"><img class="image" src="'+resultList[i].imagePath+'"></a><div class="search-result-item-body"><div class="row"><div class="col-sm-9"><h5 class="search-result-item-heading"><a href="#"> '+resultList[i].title+' </a></h5><p class="info">'+resultList[i].publishDate+'</p><p class="description">'+resultList[i].description+'</p><div class="custom-buttons"><span class="badge ">'+resultList[i].keywords+'</span></div></div></div></div></section>';
                         document.getElementById('searchSection').appendChild(newDiv);
@@ -94,8 +103,7 @@ $( document ).ready(function() {
             	}
         	});
         }
-         document.getElementById("search").value = "";
-    	 counter = counter + 1;
+		 counter = counter + 1;
         }
         else {
             var handles = document.querySelectorAll("#filter-counts, #searchSection, #pagination, #resultCount");
