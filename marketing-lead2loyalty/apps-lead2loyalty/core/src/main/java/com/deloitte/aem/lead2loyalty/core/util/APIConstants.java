a4ae1768b01487eb5d8effb7409dcd84ed808ba7
nkumartamrakar <nkumartamrakar@deloitte.com>
2023-04-04 16:48:17 +0530

    code cleaning and login scenario fixes.

---
+++ marketing-lead2loyalty/apps-lead2loyalty/core/src/main/java/com/deloitte/aem/lead2loyalty/core/util/APIConstants.java
@@ -0,0 +1,25 @@
+package com.deloitte.aem.lead2loyalty.core.util;
+
+public class APIConstants {
+
+    static String API_KEY = "client_id";
+    static String CLIENT_SECRET = "client_secret";
+    static String JWT_TOKEN = "jwt_token";
+    static String API_KEY_VALUE = "948568880bd4468096f31aecc9d02843";
+    static String CLIENT_SECRET_VALUE = "p8e-dlXIzZ3L-cfkYuP7dF3164TIb0-V-hCM";
+    static String JWT_TOKEN_VALUE = "eyJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2NzgxODYxOTEsImlzcyI6IkZENjQxNUYzNTRFRUYzMjUwQTRDOThBNEBBZG9iZU9yZyIsInN1YiI6IjFDNDQ2RjVDNjM4NzE3NEQwQTQ5NUUxOUB0ZWNoYWNjdC5hZG9iZS5jb20iLCJhdWQiOiJodHRwczovL2ltcy1uYTEuYWRvYmVsb2dpbi5jb20vYy85NDg1Njg4ODBiZDQ0NjgwOTZmMzFhZWNjOWQwMjg0MyIsImh0dHBzOi8vaW1zLW5hMS5hZG9iZWxvZ2luLmNvbS9zL2VudF9kYXRhc2VydmljZXNfc2RrIjp0cnVlfQ.ipjxCUU2g97lD7UPOBtuSwPeFsrTkj-dHWPaHEQauqEV8DKbuW8qsUjcqfbQLsqQuqyEC4ik-BXPFb7UXMXvyKmqgp5T_bojHXkvN6Vjvo801O3DmDMNb9I4QqPgWDNNy9PKDI9Tcqe0dCF2AG1PRF08Jdn5facEFnMKCf8My2eRvqrSOT1K601Mc8ompoK4XesnQqWIKYwZIlOu06ZXueJjieQ7FSqZjKvqPPfIsBdA5Kz4D_A5Tai30CS_vPX7G5QdAu1cis2dlODsXk34eISkJsYtCoMMJtM6Nzm6l-q3PxrlISpWviCpkjcAnC3i3aq-r6Dq7Y-q0bcJbko9TQ";
+    static String IMS = "ims-na1.adobelogin.com";
+    static String AUTHENTICATUION_POST_URI = "https://ims-na1.adobelogin.com/ims/exchange/jwt/";
+
+    static String HEADER_KEY_ACCEPT = "Accept";
+    static String HEADER_KEY_ACCEPT_VALUE = "application/json";
+    static String HEADER_KEY_AUTHORIZATION = "Authorization";
+    static String HEADER_KEY_AUTHORIZATION_VALUE = "";
+    static String HEADER_KEY_X_API_KEY = "x-api-key";
+    static String HEADER_KEY_X_API_KEY_VALUE = API_KEY_VALUE;
+    static String HEADER_KEY_X_GW_IMS_ORG_ID = "x-gw-ims-org-id";
+    static String HEADER_KEY_X_GW_IMS_ORG_ID_VALUE = "FD6415F354EEF3250A4C98A4@AdobeOrg";
+    static String HEADER_KEY_X_SANDBOX_NAME = "x-sandbox-name";
+    static String HEADER_KEY_X_SANDBOX_NAME_VALUE = "l2l-b2b";
+
+}
