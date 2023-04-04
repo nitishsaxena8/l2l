228d6cd73320c74fb6e08167652f689253724d68
nkumartamrakar <nkumartamrakar@deloitte.com>
2023-04-04 16:49:00 +0530

    code cleaning and login scenario fixes.

---
+++ marketing-lead2loyalty/apps-lead2loyalty/core/src/main/java/com/deloitte/aem/lead2loyalty/core/constants/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package com.deloitte.aem.lead2loyalty.core.constants;
+
+public class ApplicationConstants {
+
+    public static final String LOYALTY_USER_PATH = "/var/lead2loyalty-users/";
+    public static final String USER_EMAIL_COOKIE = "userEmail";
+    public static final String VAR_NODE_PATH = "/var";
+    public static final String BOOKMARKS_PROPERTY = "bookmarks";
+    public static final String PAGE_TYPE_PROPERTY = "pageType";
+    public static final String IMAGE_PROPERTY = "image";
+    public static final String CQ_LAST_MODIFIED_PROPERTY = "cq:lastModified";
+
+}
